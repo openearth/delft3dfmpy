1935c9573b4c6a1bedb7daa83d07c710685fb344
grongen <guusrongen@gmail.com>
2021-01-05 12:15:57 +0100

    Add option for only reading 2d mesh from Ugrid 0.9 file
    The function "from_netcdf_old" reads a mesh from a Ugrid 0.9 convention file (with NetNode and NetLink variables). This function is meant for the 2D mesh, it does not distinguish based on NetLinkType. Now the option only2d is added, with which the non 2d part cna be skipped, in case of importing a 1d2d mesh.

--- delft3dfmpy/io/gridio.py
+++ delft3dfmpy/io/gridio.py
@@ -1,5 +1,13 @@
 import netCDF4
 import numpy as np
+import sys
+sys.path.append('D:/Documents/GitHub/delft3dfmpy')
+from delft3dfmpy.datamodels.cstructures import meshgeom, meshgeomdim
+
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 def to_netcdf_old(meshgeom, path):
 
@@ -8,7 +16,7 @@ def to_netcdf_old(meshgeom, path):
 
     ncfile.createDimension("nNetNode", meshgeom.meshgeomdim.numnode)
     ncfile.createDimension("nNetLink", meshgeom.meshgeomdim.numedge)
-    ncfile.createDimension("Two", 2)
+    ncfile.createDimension("nNetLinkPts", 2)
 
     # Mesh2D
     mesh2d = ncfile.createVariable("Mesh2D", "i4", ())
@@ -32,27 +40,58 @@ def to_netcdf_old(meshgeom, path):
         else:
             ncvar[:] = meshgeom.get_values(f'node{dim}')
 
-
     # Links
-    ncvar = ncfile.createVariable("NetLink", "i4", ( mesh2d.edge_dimension, "Two"))
+    ncvar = ncfile.createVariable("NetLink", "i4", ( mesh2d.edge_dimension, "nNetLinkPts"))
     links = meshgeom.get_values('edge_nodes', as_array=True)
     ncvar[:] = links.tolist()
-    # ncvar
-
+    
+    # NetLinkType
     ncvar = ncfile.createVariable("NetLinkType", "i4", mesh2d.edge_dimension)
     ncvar[:] = np.ones(len(links), dtype=int) * 2
 
     ncfile.close()
 
-def from_netcdf_old(meshgeom, path):
+def from_netcdf_old(meshgeom, path, only2d=False):
+    """
+    Method to read mesh from 'old' netcdf 0.9 format
+    Function only suitable for 
+
+    Parameters
+    ----------
+    meshgeom : [type]
+        [description]
+    path : str
+        Path to netcdf with mesh
+    """
+
     ds = netCDF4.Dataset(path, 'r')
 
-    meshgeom.meshgeomdim.numnode = ds.dimensions['nNetNode'].size
-    meshgeom.meshgeomdim.numedge = ds.dimensions['nNetLink'].size
+    # Get netlinktype
+    netlinktype = ds.variables['NetLinkType'][:].data
+    links = ds.variables['NetLink'][:, :] - 1
+
+    if only2d and (netlinktype != 2).any():
+        linkidx = netlinktype == 2
+        nodeidx = np.unique(links[linkidx, :])
+        links = links[linkidx, :]
+
+        meshgeom.meshgeomdim.numnode = len(nodeidx)
+        meshgeom.meshgeomdim.numedge = sum(linkidx)
+
+        # The links should be renumbered, to compensate the missing ones
+        id_mapping = {old_id: new_id for new_id, old_id in enumerate(nodeidx)}
+        links = np.reshape([id_mapping[old_id] for old_id in links.ravel()], links.shape)
+
+    else:
+        # Get dimensions
+        meshgeom.meshgeomdim.numnode = ds.dimensions['nNetNode'].size
+        meshgeom.meshgeomdim.numedge = ds.dimensions['nNetLink'].size
+        
+        nodeidx = slice(None)
 
     for dim in list('xyz'):
         # Get values
-        data = ds.variables[f'NetNode_{dim}'][:]
+        data = ds.variables[f'NetNode_{dim}'][nodeidx]
         # Allocate
         meshgeom.allocate(f'node{dim}')
         # Set values
@@ -60,6 +99,6 @@ def from_netcdf_old(meshgeom, path):
 
     # Set links
     meshgeom.allocate(f'edge_nodes')
-    meshgeom.set_values('edge_nodes', ds.variables['NetLink'][:, :].ravel())
+    meshgeom.set_values('edge_nodes', links.ravel() + 1)
 
     ds.close()
